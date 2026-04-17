3b967e9dc52d98e1f5ab62d2786b59f7457f7a80
Nehal Adil <mdnehaladil@gmail.com>
2026-04-17 20:28:18 +0530

    deployment error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Navbar from "@/screens/navbar/Navbar";
+import {
+  Navbar,
+  Home,
+  Benefits,
+  OurClasses,
+  ContactUs,
+  Evolve,
+  Footer,
+} from "@/screens";
 import { useEffect, useState } from "react";
 import { SelectedPageEnum, type SelectedPage } from "./shared/types";
-import Footer from "@/screens/footer/footer";
-import Home from "@/screens/home/Home";
-import Benefits from "@/screens/benefits/Benefits";
-import Evolve from "@/screens/whyEvolve/Evolve";
-import OurClasses from "@/screens/ourClasses/OurClasses";
 import RollingRibbon from "@/shared/RollingRibbon";
-import ContactUs from "@/screens/contacts/ContactUs";
 
 const App = () => {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
---
+++ src/screens/index.ts
@@ -0,0 +1,9 @@
+import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
+import Benefits from "./benefits/Benefits";
+import Footer from "./footer/footer";
+import ContactUs from "./contacts/ContactUs";
+import OurClasses from "./ourClasses/OurClasses";
+import Evolve from "./whyEvolve/Evolve";
+
+export { Navbar, Home, Benefits, Footer, ContactUs, OurClasses, Evolve };
