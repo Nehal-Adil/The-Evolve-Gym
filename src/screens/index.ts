77188fb2265a07e0efb85ad3277c30e0c11839e2
Nehal Adil <mdnehaladil@gmail.com>
2026-04-18 20:10:16 +0530

    deployment error

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,7 +3,7 @@ import Home from "./home/Home";
 import Benefits from "./benefits/Benefits";
 import Footer from "./footer/footer";
 import ContactUs from "./contacts/ContactUs";
-import Classes from "./ourclasses/Classes";
+import Classes from "./classes/Classes";
 import Evolve from "./whyEvolve/Evolve";
 
 export { Navbar, Home, Benefits, Footer, ContactUs, Classes, Evolve };
